3f602a550ee4ac3f729eaca013f031dca3ce2ca1
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-06-14 14:17:57 +0200

    Initial commit with shared components iteration of MixturesApp

---
+++ src/components/ResultBox.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <!-- text between -->
+    <p v-text="'And the result...'" />
+
+    <!-- mixture effect -->
+    <div class="flask__wrapper">
+      <flask-item
+      :color="mixtureEffectFill"
+      amount="100"
+      :size="15" />
+    </div>
+
+    <!-- refresh btn -->
+    <button-item
+      @click="$emit('refresh')"
+      :size="4"
+      :movement="-0.5"
+      :font-size="1.5"
+      icon="sync" />
+  </div>
+</template>
+
+<script>
+import FlaskItem from './shared/FlaskItem'
+import ButtonItem from './shared/ButtonItem'
+
+export default {
+  name: 'ResultsBox',
+  props: {
+    mixtures: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    mixtureEffectFill () {
+      const [redCol, greenCol, blueCol] = this.mixtures.map(item => Math.floor(item.amount * 2.5))
+      return `rgb(${redCol}, ${greenCol}, ${blueCol})`
+    }
+  },
+  components: {
+    FlaskItem,
+    ButtonItem
+  }
+}
+</script>
+
+<style lang="scss">
+.flask__wrapper {
+  width: fit-content;
+  font-size: 20rem;
+  margin: 0 auto;
+}
+
+.refresh-btn {
+  background-color: #9a9a9a;
+  background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
+  width: 4rem;
+  height: 4rem;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  -webkit-box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
+  box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
+  transition: .3s;
+  outline: none;
+  font-size: 1.5rem;
+  color: #637892;
+
+  &:hover {
+    margin-top: -0.5rem;
+  }
+}
+</style>
