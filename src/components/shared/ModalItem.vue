0e959accc2555d0d3b66a3ca123d421515cddc74
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-06-14 15:21:17 +0200

    Added ModalItem.vue without use them

---
+++ src/components/shared/ModalItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="fog">
+    <div class="modal">
+
+      <header>
+        <h1>
+          <slot name="header" />
+        </h1>
+      </header>
+
+      <div class="modal__body">
+        <slot name="body" />
+      </div>
+
+      <footer class="modal__footer">
+        <slot name="footer" />
+        <button-item
+          icon="fa-times"
+          @click="$emit('cancel')" />
+      </footer>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonItem from './ButtonItem'
+
+export default {
+  name: 'ModalItem',
+  components: {
+    ButtonItem
+  }
+}
+</script>
